2727a701bcd3c8a37eaadcabbe020b5753814f14
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2024-05-19 13:43:01 +0530

    Update linkedlist_cycle-detection.cpp

--- implementation_DSA/linkedlist_cycle-detection.cpp
+++ implementation_DSA/linkedlist_cycle-detection.cpp
@@ -12,11 +12,9 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (head==nullptr) return false;
-        if(head->next == nullptr) return false;
         ListNode* a=head;
         ListNode* b=head;
-        while(a!=nullptr && b!=nullptr && b->next!=nullptr){
+        while(b!=nullptr && b->next!=nullptr){
             a=a->next;
             b=b->next->next;
             if (a==b) return true;
