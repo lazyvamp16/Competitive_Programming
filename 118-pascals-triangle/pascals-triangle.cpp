ad68b70cd2d6c4fde679aebae3040b12fa9bf9f0
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-07-16 02:18:24 +0530

    Time: 13 ms (0.70%) | Memory: 9.6 MB (62.89%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> ans;
+        for(int i =0; i<n; i++){
+            vector<int> row(i+1);
+            for(int j=0; j<=i;j++){
+                cout <<"i"<<  i << endl;
+                if(j==0 || j==i) row[j]=1;
+                else row[j] = ans[i-1][j-1]+ans[i-1][j];
+            }
+            ans.push_back(row);
+        }
+        return ans;
+    }
+};
