3afa945f3c8578e40342785a76f037288d363bbc
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-07-16 20:02:00 +0530

    Time: 4 ms (65.29%) | Memory: 96.1 MB (62.35%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        int allEven=0, allOdd=0, oddEven=0, evenOdd=0;
+        int allEven=0, allOdd=0, alternate=1; 
+        //int oddEven=0, evenOdd=0;
         for(int i=0; i<nums.size(); i++){
-            if(nums[i]%2==0) {allEven++;}
-            else if(nums[i]%2==1) {allOdd++;}
+            if(nums[i]%2==0) {
+                allEven++;
+            }
+            else if(nums[i]%2==1) {
+                allOdd++;
+            }
+            if(i>0 && ((nums[i]+nums[i-1])%2==1)) {
+                alternate++;
+            }
             
             //if(i%2==0 && nums[i]%2==0) {evenOdd++;}
             //else if(i%2==1 && nums[i]%2==1) {evenOdd++;}
@@ -12,15 +20,8 @@ public:
             //if(i%2==0 && nums[i]%2==1) {oddEven++;}
             //else if(i%2==1 && nums[i]%2==0) {oddEven++;}
         }
-
-        int cnt=1;
-        for(int i=1; i<nums.size();i++){
-            if((nums[i]+nums[i-1])%2==1) cnt++;
-        }
-        
-        int ans1 = max(allEven,allOdd);
+        int ans = max(allEven,allOdd);
         //int ans2 = max(evenOdd, oddEven);
-        //cout << allEven << " " << allOdd << " " << evenOdd << " " << oddEven << endl;
-        return max(ans1,cnt);
+        return max(ans,alternate);
     }
 };
