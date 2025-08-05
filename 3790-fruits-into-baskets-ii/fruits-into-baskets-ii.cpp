e62e76d7a5f7adcb3074abd6d7eaf316b05ee7b1
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-08-05 12:01:23 +0530

    Time: 0 ms (100.00%) | Memory: 48.9 MB (49.69%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
         int cnt =0;
+        //unordered_set<int> used;
         for(int i : fruits){
             bool flag = false;
             for(int j=0;j<baskets.size();j++){
+                //if(baskets[j]>=i && !used.count(j)){
                 if(baskets[j]>=i){
+                    //used.insert(j);
                     flag =true;
                     baskets.erase(baskets.begin()+j);
                     break;
                 }
             }
-            for(int k:baskets) cout << k << " ";
-            cout <<endl;
+            //for(int k:baskets) cout << k << " ";
+            //cout <<endl;
             if(!flag) cnt++;
         }
         return cnt;
