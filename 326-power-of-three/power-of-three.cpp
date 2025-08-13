ff50050b1cf5d1907bd29f1bed01e3d58f0d5c3e
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-08-13 12:32:30 +0530

    Time: 6 ms (27.44%) | Memory: 8.8 MB (76.82%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n<1) return false;
-        while(n>0){
-            if(n==1) return true;
-            if(n%3==0) n=n/3;
-            else return false;
-        }
-        return true;
+        int maxPowerOf3 = 1162261467;
+        return (n > 0 && ((maxPowerOf3 % n) == 0));
     }
 };
