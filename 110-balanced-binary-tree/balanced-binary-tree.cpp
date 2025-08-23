97bb9a39629b187c2f5c9879ed556bdbd6d34db7
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-08-23 16:01:48 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (97.74%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,18 +11,31 @@
  */
 class Solution {
 private:
-    int mh(TreeNode* node){
+    int maxheight(TreeNode* node){
         if(!node) return 0;
-        int left = mh(node->left);
-        int right = mh(node->right);
+        int left = maxheight(node->left);
+        int right = maxheight(node->right);
         return(1 + max(left,right));
     }
+
+    int check(TreeNode* node){
+        if(!node) return 0;
+        int left = check(node->left);
+        if(left==-1) return -1;
+        int right = check(node->right);
+        if(right==-1) return -1;
+        if(abs(left-right)>1) return -1;
+        return(1 + max(left,right));
+    }
+
 public:
     bool isBalanced(TreeNode* root) {
-        if(!root) return true;
-        int left = mh(root->left);
-        int right = mh(root->right);
-        if(abs(left-right)>1) return false;
-        return (isBalanced(root->left) && isBalanced(root->right));
+        // if(!root) return true;
+        // int left = maxheight(root->left);
+        // int right = maxheight(root->right);
+        // if(abs(left-right)>1) return false;
+        // return (isBalanced(root->left) && isBalanced(root->right));
+        if(check(root)==-1) return false;
+        return true;
     }
 };
