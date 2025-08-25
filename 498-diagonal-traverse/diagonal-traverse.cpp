7c634d357e37366289599cfe1d90e3886b52e5c1
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-08-25 11:09:30 +0530

    Time: 208 ms (5.10%) | Memory: 25.7 MB (44.08%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        int num;
+        if(m==1) num = n;
+        else if(n==1) num = m;
+        else  num = ((m*n)/2) +1; 
+        //if(((m*n)%2) != 0) num+=1;
+        //int sum = num/2;
+        //if(num%2==0) sum -=1;
+        vector<int> ans;
+        for(int i=0; i<num; i++){
+            vector<int> v;
+            for(int j=0; j<m; j++){
+                if(j>i) break;
+                for(int k=0; k<n; k++){
+                    if(k>i) break;
+                    if(j+k==i) v.push_back(mat[j][k]);
+                }
+            }
+            if(i%2 ==0) reverse(v.begin(),v.end());
+            for (int x : v) ans.push_back(x);
+        }
+        return ans;
+    }
+};
+
