dff681fb141441ee1bded076bb9dfcd145a7e30b
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-09-11 12:02:21 +0530

    Time: 29 ms (11.13%) | Memory: 22.3 MB (5.44%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     string sortVowels(string &s) {
-        multiset<int> ms;
+        //multiset<int> ms;
+        vector<char> ms;
         vector<int> ind;
         unordered_set<char> vowels={'a','e','i','o','u',
                                     'A','E','I','O','U'};
@@ -9,11 +10,13 @@ public:
         int i=0;
         for(char c : s){
             if(vowels.count(c)!=0){
-                ms.insert(int(c));
+                //ms.insert(int(c));
+                ms.push_back(c);
                 ind.push_back(i);
             }
             i++;
         }
+        sort(ms.begin(),ms.end());
         i=0;
         for(auto c: ms){
             s[ind[i]] = c;
