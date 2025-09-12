66785a8e31b8bbd16aff74d5d859f3adbabcc439
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-09-12 11:00:52 +0530

    Time: 2 ms (89.18%) | Memory: 19.1 MB (25.75%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     bool doesAliceWin(string s) {
         int cnt =0;
-        unordered_set<char> vowels={'a','e','i','o','u'};
-        for(char c : s){
-            if(vowels.count(c)!=0) cnt++;
-        }
-        if(cnt==0) return false;
-        return true;
+        vector<char> vowels={'a','e','i','o','u'};
+        for(char c : s) for(char v : vowels) if(v==c) cnt++;
+        return (cnt!=0);
     }
 };
