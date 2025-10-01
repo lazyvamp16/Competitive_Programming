9069b6b9c624d48ed6ec4398b8a9d0ea942ef613
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-10-01 21:26:32 +0530

    Time: 95 ms (40.76%) | Memory: 81.5 MB (50.36%) - LeetSync

---
+++ 1397-search-suggestions-system/search-suggestions-system.cpp
@@ -0,0 +1,71 @@
+class Trie
+{
+    struct Node{
+        vector<Node*> children{vector<Node*>(26,NULL)} ;
+        bool isWord = false;
+    };
+    Node *Root,*curr;
+
+public:
+    Trie(){
+        Root = new Node();
+    }
+
+    void insert(string &s){
+        curr = Root;
+        for(char c: s){
+            if(!curr->children[c-'a']){
+                curr->children[c-'a'] = new Node();
+            }
+            curr = curr->children[c-'a'];
+        }
+        curr->isWord = true;
+    }
+
+    void dfs(Node * curr, string & word, vector<string> & result){
+        if(result.size()==3) return;
+        if(curr->isWord) result.push_back(word);
+
+        for(char c='a'; c<='z'; c++){
+            if(curr->children[c-'a']){
+                word +=c;
+                dfs(curr->children[c-'a'],word,result);
+                word.pop_back();
+            }
+        }
+    }
+
+    vector<string> getWordsStartingWith(string &prefix) {
+        curr = Root;
+        vector<string> result;
+        for(char c : prefix){
+            if(!curr->children[c-'a']) return result;
+            curr = curr->children[c-'a'];
+        }
+        dfs(curr,prefix,result);
+        return result;
+
+    }
+};
+
+
+
+
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        
+        Trie trie = Trie();
+        vector<vector<string>> result;
+
+        for(string w: products){
+            trie.insert(w);
+        }
+        string prefix="";
+        for(char c: searchWord){
+            prefix +=c;
+            result.push_back(trie.getWordsStartingWith(prefix));
+        
+        }return result;
+    }
+};
