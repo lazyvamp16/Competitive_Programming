30222a8e271e98afe67e682af1a6e6a0fe528272
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-10-10 13:00:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (31.12%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        for(char c : s){
+            if(!st.empty() && st.top()!=c && c==')') st.pop();
+            else st.push(c);
+        }
+        return st.size();
+    }
+};
