a014c78780afeab35e674daa118af10e772c6250
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-10-18 11:51:00 +0530

    Time: 133 ms (30.91%) | Memory: 114.9 MB (27.76%) - LeetSync

--- 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.cpp
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.cpp
@@ -5,6 +5,7 @@ public:
         if(k>=(n/2)) return n;
         sort(v.begin(),v.end());
         unordered_set<int> s;
+        int cnt = 0;
         for(int i=0; i<n; i++){
             if(!s.count(v[i]-k)){
                 s.insert(v[i]-k);
@@ -13,12 +14,14 @@ public:
             else {
                 if(v[i-1]+1>v[i]+k) {
                     v[i] = v[i-1];
+                    cnt++;
                     continue;
                 }
                 v[i] = v[i-1]+1;
                 s.insert(v[i]);
             }
         }
+        /*
         s.clear();
         int cnt =0;
         for(int i: v){
@@ -26,6 +29,7 @@ public:
             if(s.count(i)) cnt++;
             else s.insert(i);
         }
+        */
         return n-cnt;
     }
 };
