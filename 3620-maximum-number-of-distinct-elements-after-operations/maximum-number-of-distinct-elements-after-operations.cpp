8cb5cfca729796470e058a31621e512b627824b6
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-10-18 11:50:16 +0530

    Time: 264 ms (18.30%) | Memory: 125.3 MB (26.50%) - LeetSync

---
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& v, int k) {
+        int n= v.size();
+        if(k>=(n/2)) return n;
+        sort(v.begin(),v.end());
+        unordered_set<int> s;
+        for(int i=0; i<n; i++){
+            if(!s.count(v[i]-k)){
+                s.insert(v[i]-k);
+                v[i] = v[i]-k;
+            }
+            else {
+                if(v[i-1]+1>v[i]+k) {
+                    v[i] = v[i-1];
+                    continue;
+                }
+                v[i] = v[i-1]+1;
+                s.insert(v[i]);
+            }
+        }
+        s.clear();
+        int cnt =0;
+        for(int i: v){
+            cout << i << " ";
+            if(s.count(i)) cnt++;
+            else s.insert(i);
+        }
+        return n-cnt;
+    }
+};
