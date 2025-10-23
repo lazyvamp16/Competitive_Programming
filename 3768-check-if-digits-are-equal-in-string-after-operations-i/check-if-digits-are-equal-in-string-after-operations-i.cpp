d6a6cbad692f8fcacf264241d3efbd1f224fec13
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-10-23 12:31:14 +0530

    Time: 4 ms (75.59%) | Memory: 10.2 MB (36.70%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -4,7 +4,7 @@ public:
         int n = s.size();
         for(int i=0; i<n-2; i++){
             string t="";
-            cout << s << endl;
+            //cout << s << endl;
             for(int j=1; j<s.size(); j++){
                 int d= (s[j]-'0'+s[j-1]-'0')%10;
                 //cout << d << " ";
@@ -12,7 +12,7 @@ public:
             }
             s = t;
         }
-        cout << s;
+        //cout << s;
         if(s.size()<2) return false;
         if(s[0]==s[1]) return true;
         return false;
