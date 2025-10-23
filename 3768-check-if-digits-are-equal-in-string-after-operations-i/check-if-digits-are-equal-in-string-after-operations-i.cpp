dd3fddfec7e00f73415742532650e2e855ae142d
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-10-23 12:31:00 +0530

    Time: 36 ms (8.42%) | Memory: 10.1 MB (49.83%) - LeetSync

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasSameDigits(string s){
+        int n = s.size();
+        for(int i=0; i<n-2; i++){
+            string t="";
+            cout << s << endl;
+            for(int j=1; j<s.size(); j++){
+                int d= (s[j]-'0'+s[j-1]-'0')%10;
+                //cout << d << " ";
+                t.push_back(d+'0');
+            }
+            s = t;
+        }
+        cout << s;
+        if(s.size()<2) return false;
+        if(s[0]==s[1]) return true;
+        return false;
+    }
+};
