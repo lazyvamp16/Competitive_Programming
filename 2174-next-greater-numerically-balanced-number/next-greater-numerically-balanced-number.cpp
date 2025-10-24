c82703a7bff8e0192b43a40ad2c1fb231f622faf
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-10-24 13:01:14 +0530

    Time: 331 ms (35.68%) | Memory: 148.2 MB (30.65%) - LeetSync

--- 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int nextBeautifulNumber(int n) {
         for(int i=n+1; i<=1e6; i++ ){
-            unordered_map<int,int> m;
+            vector<int> m(10);
             int temp = i;
             while(temp>0){
                 int digit = temp%10;
@@ -10,9 +10,12 @@ public:
                 temp/=10;
             }
             bool flag = true;
-            for(auto i:m){
-                if(i.second!=i.first) flag = false;
+            for(int i=0; i<10; i++){
+                if(m[i]>0 && m[i]!=i) flag = false;
             }
+            // for(auto i:m){
+            //     if(i.second!=i.first) flag = false;
+            // }
             if(flag){
                 return i;
             }
