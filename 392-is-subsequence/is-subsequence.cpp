70407d4923e7de494160d097a4fc911186a30634
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-11-03 12:04:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (10.45%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0,j=0;
+        while(i<s.length()){
+            if(j>=t.length() && i!=s.length()){
+                return false;
+            }
+            if(s[i]==t[j]) {
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+
+        }
+        return true;
+    }
+};
