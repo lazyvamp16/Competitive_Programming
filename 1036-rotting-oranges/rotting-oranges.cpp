c5479da577a4e28278e22412eae9601e3d0c1b1a
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-11-17 17:18:38 +0530

    Time: 3 ms (29.44%) | Memory: 17.6 MB (10.75%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<vector<int>> q;
+        int m = grid.size();
+        int n= grid[0].size();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==2) {
+                    q.push({i,j,0});
+                }
+            }
+        }
+        vector<int> x{0,0,-1,1};
+        vector<int> y{1,-1,0,0};
+        int ans =0;
+        while(!q.empty()){
+            vector<int> top = q.front();
+            q.pop();
+            int i=top[0];
+            int j=top[1];
+            int t=top[2];
+            ans = max(ans,t);
+            for(int k=0; k<4; k++){
+                int ni=i+x[k];
+                int nj=j+y[k];
+                if(ni>=0 && ni<m && nj>=0 && nj<n
+                && grid[ni][nj]==1){
+                    grid[ni][nj]=2;
+                    q.push({ni,nj,t+1});
+                }
+            }
+        }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==1) return -1;
+            }
+        }
+        return ans;
+    }
+};
