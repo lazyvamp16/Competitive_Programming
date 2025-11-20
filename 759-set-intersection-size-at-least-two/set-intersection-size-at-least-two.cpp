98d9a221de4184f7e9766957b77497d8ecb91041
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-11-20 12:56:57 +0530

    Time: 3 ms (81.07%) | Memory: 22.7 MB (16.05%) - LeetSync

--- 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.cpp
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int intersectionSizeTwo(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
-        return a[1] < b[1];
-    });
+        sort(intervals.begin(), intervals.end(),
+            [&](const vector<int> &a, const vector<int> &b){
+                return a[1]<b[1];
+            }
+        );
         vector<int> ans;
         set<int> s;
 
