94b1e21dee02a4c70f5aa9b3011c8b8682206f7e
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-11-25 15:17:31 +0530

    Time: 127 ms (5.50%) | Memory: 7.7 MB (81.50%) - LeetSync

---
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        long long num =1;
+        int cnt =1;
+        while(cnt<1e6){
+            if(cnt>17) num=num%k;
+            if(num%k==0) return cnt;
+            num = num*10 +1;
+            cnt++;
+        }
+        return -1;
+    }
+};
