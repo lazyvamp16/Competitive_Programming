ddc5ffcd6fe65e25bccaa7afa32d16ce9062114e
Neev Mehta <91076236+lazyvamp16@users.noreply.github.com>
2025-12-08 20:19:31 +0530

    Time: 11 ms (44.91%) | Memory: 7.9 MB (21.68%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countTriples(int n) {
+        int ans =0;
+        for(int i=1; i<n; i++){
+            for(int j=1; j<n; j++){
+                int sq = i*i + j*j ;
+                double root = sqrt(sq);
+                if(root == floor(root) && root<=n) {
+                    //cout << i << " "<<j << " " <<root << " "<< sq <<endl;
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
