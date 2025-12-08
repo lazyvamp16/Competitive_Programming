22d2c5215107e1001d340d4d2c31a82daaa7afe6
Neev Mehta <91076236+lazyvamp16@users.noreply.github.com>
2025-12-08 20:22:36 +0530

    Time: 7 ms (62.17%) | Memory: 7.9 MB (55.75%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -3,12 +3,12 @@ public:
     int countTriples(int n) {
         int ans =0;
         for(int i=1; i<n; i++){
-            for(int j=1; j<n; j++){
+            for(int j=i+1; j<n; j++){
                 int sq = i*i + j*j ;
                 double root = sqrt(sq);
                 if(root == floor(root) && root<=n) {
                     //cout << i << " "<<j << " " <<root << " "<< sq <<endl;
-                    ans++;
+                    ans+=2;
                 }
             }
         }
